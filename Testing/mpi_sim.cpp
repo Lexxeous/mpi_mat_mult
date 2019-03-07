334bf7eef8aae23260d86b18cb58ac47de833e9c
Jonathan Alexander Gibson <agibson@gibsonusa.net>
2019-03-07 16:47:11 -0600

    Starting data collection and organization

--- Testing/mpi_sim.cpp
+++ Testing/mpi_sim.cpp
@@ -16,8 +16,11 @@ double range_rand_double(double low, double high);
 // start main
 int main(int argc, char* argv[])
 {
+	// define necessary variables
 	int mat_size = atoi(argv[1]); // user specified matrix size
-	double C[mat_size][mat_size]; // create C as a "global" matrix?
+	double C[mat_size][mat_size]; // create C as a global matrix
+	double start_time, make_time, mult_time; // scope the time variables
+
 
 	// initialize the MPI environment
   MPI_Init(NULL, NULL);
@@ -25,8 +28,8 @@ int main(int argc, char* argv[])
   MPI_Comm_size(MPI_COMM_WORLD, &world_size); // get the number of processes
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank); // get the ranks for each individual process
 
-  double start_time, make_time, mult_time;
-
+  
+  // only process 0 tracks the execution time
   if(world_rank == 0)
   {
   	start_time = MPI_Wtime(); // wall time at the start of the program	
@@ -41,7 +44,7 @@ int main(int argc, char* argv[])
   }
 
 
-  // defining necessary variables
+  // define additional necessary variables
 	int chunk_size = floor(mat_size / world_size);
 	int unique_rows = (mat_size % world_size) + chunk_size;
 	double double_lower_bound = 0;
@@ -57,70 +60,11 @@ int main(int argc, char* argv[])
   	cout << "\nCreating square matricies of size " << mat_size << "...\n";
   }
   MPI_Barrier(MPI_COMM_WORLD);
-	
-
-	// // every process gets there own instance of matrices A & B
-	// double A[10][10] = {0,0,0,0,0,0,0,0,0,0,
-	// 								 1,1,1,1,1,1,1,1,1,1,
-	// 								 2,2,2,2,2,2,2,2,2,2,
-	// 								 3,3,3,3,3,3,3,3,3,3,
-	// 								 4,4,4,4,4,4,4,4,4,4,
-	// 								 5,5,5,5,5,5,5,5,5,5,
-	// 								 6,6,6,6,6,6,6,6,6,6,
-	// 								 7,7,7,7,7,7,7,7,7,7,
-	// 								 8,8,8,8,8,8,8,8,8,8,
-	// 								 9,9,9,9,9,9,9,9,9,9};
-
- //  double B[10][10] = {0,0,0,0,0,0,0,0,0,0,
-	// 								 1,1,1,1,1,1,1,1,1,1,
-	// 								 2,2,2,2,2,2,2,2,2,2,
-	// 								 3,3,3,3,3,3,3,3,3,3,
-	// 								 4,4,4,4,4,4,4,4,4,4,
-	// 								 5,5,5,5,5,5,5,5,5,5,
-	// 								 6,6,6,6,6,6,6,6,6,6,
-	// 								 7,7,7,7,7,7,7,7,7,7,
-	// 								 8,8,8,8,8,8,8,8,8,8,
-	// 								 9,9,9,9,9,9,9,9,9,9};
 
 
-	
+	// every process gets its own instance of matrices A & B
 	double A[mat_size][mat_size];
 	double B[mat_size][mat_size];
-	// double temp_C[mat_size][mat_size];
-
-	// // allocating the matrices
-	// cout << endl << "Creating square matricies of size 10 with random double values..." << endl;
-	// double** A, B, C;
-	// A = calloc(10, sizeof(double*));
-	// B = calloc(10, sizeof(double*));
-	// C = calloc(10, sizeof(double*));
-	// for (int i = 0; i < 10; i++)
-	// {
-	//   A[i] = calloc(10, sizeof(double));
-	//   B[i] = calloc(10, sizeof(double));
-	//   temp_C[i] = calloc(10, sizeof(double));
-	// }
-
-	// double** A, B, C;
-	// A = new double*[mat_size];
-	// B = new double*[mat_size];
-	// C = new double*[mat_size];
-	// for(int i = 0; i < mat_size; ++i)
-	// {
-	//   A[i] = new double[mat_size];
-	//   B[i] = new double[mat_size];
-	//   temp_C[i] = new double[mat_size];
-	// }
-
-	// double** A = (double**) (malloc(sizeof(double*) * 10));
-	// double** B = (double**) (malloc(sizeof(double*) * 10));
-	// double** C = (double**) (malloc(sizeof(double*) * 10));
-	// for (int i = 0; i < 10; i++)
-	// {
-	//   A[i] = (double*) malloc(sizeof(double) * 10);
-	//   B[i] = (double*) malloc(sizeof(double) * 10);
-	//   temp_C[i] = (double*) malloc(sizeof(double) * 10);
-	// }
 
 
 	if(world_rank == 0)
@@ -140,37 +84,12 @@ int main(int argc, char* argv[])
 		}
 	}
 
- //  // print the original matrices only once
- //  if(world_rank == 0)
- //  {
-	// 	// print matrix A								 
-	// 	for(int a_row = 0; a_row < mat_size; a_row++)
-	// 	{
-	// 		for(int a_col = 0; a_col < mat_size; a_col++)
-	// 		{
-	// 			cout << A[a_row][a_col] << "\t";
-	// 		}
-	// 		cout << "\n";
-	// 	}
-	// 	cout << "\n";
 
-
-	// 	// print matrix B							 
-	// 	for(int b_row = 0; b_row < mat_size; b_row++)
-	// 	{
-	// 		for(int b_col = 0; b_col < mat_size; b_col++)
-	// 		{
-	// 			cout << B[b_row][b_col] << "\t";
-	// 		}
-	// 		cout << "\n";
-	// 	}
-	// 	cout << "\n";
-
+	// only process 0 tracks the execution time
 	if(world_rank == 0)
 	{
 		make_time = MPI_Wtime() - start_time; // seqential creation and initialization time
 	}
-	MPI_Barrier(MPI_COMM_WORLD);
 
 
 	// calculate start and end values for row wise partitioning for every process
@@ -219,6 +138,7 @@ int main(int argc, char* argv[])
 	}
 
 
+	// only process 0 tracks the execution time and prints to the screen
 	if(world_rank == 0)
 	{
 	  mult_time = MPI_Wtime() - make_time - start_time; // matrix mutiplication time
